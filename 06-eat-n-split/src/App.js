7dcf301d10fc1de482e2795bfb22b06604e2af2b
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-05-20 11:45:16 +0200

    [Eat-n-Split] Use key for Split Bill form

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -68,7 +68,7 @@ export default function App() {
         <Button onClick={handleShowAddFriend}>  {showAddFriend ? 'Close' : 'Add friend'}</Button>
       </div>
 
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} />}
+      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} key={selectedFriend.id} />}
     </div>
   )
 }
