f5d659214a9900d87a7ed1c059ad74168543240e
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-06-11 12:13:21 +0200

    [Section 13] 172. Creating useKey

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import StarRating from './StarRating'
 import { useMovies } from "./useMovies";
 import { useLocalStorageState } from "./useLocalStorageState";
+import { useKey } from "./useKey";
 
 // const tempMovieData = [
 // {
@@ -151,20 +152,11 @@ function Logo() {
 function Search({ query, setQuery }) {
   const inputEl = useRef(null)
 
-  useEffect(function () {
-    function callback(e) {
-      if (document.activeElement === inputEl.current) return
-
-      if (e.code === 'Enter') {
-        console.log(inputEl.current);
-        inputEl.current.focus()
-        setQuery('')
-      }
-    }
-
-    document.addEventListener('keydown', callback)
-    return () => document.addEventListener('keydown', callback)
-  }, [setQuery])
+  useKey('Enter', function () {
+    if (document.activeElement === inputEl.current) return
+    inputEl.current.focus()
+    setQuery('')
+  })
 
   return (
     <input
@@ -347,21 +339,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     [title]
   );
 
-  useEffect( // Close on Esc
-    function () {
-      function callback(e) {
-        if (e.code === 'Escape') {
-          onCloseMovie();
-        }
-      }
-      document.addEventListener('keydown', callback)
-
-      return function () {
-        document.removeEventListener('keydown', callback)
-      }
-    },
-    [onCloseMovie]
-  );
+  useKey('Escape', onCloseMovie)
 
   return (
     <div className='details'>
