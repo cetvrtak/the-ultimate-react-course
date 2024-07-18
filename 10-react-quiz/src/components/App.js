09d05cbde2f8c70ca597e7f0894ff5db4cf73b57
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-07-18 09:53:05 +0200

    [Section 16] 197. Displaying Progress

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -3,8 +3,10 @@ import Error from './Error';
 import Header from './Header';
 import Loader from './Loader';
 import Main from './Main';
+import NextButton from './NextButton';
 import StartScreen from './StartScreen';
 import Question from './Question';
+import Progress from './Progress';
 
 const initialState = {
   questions: [],
@@ -12,6 +14,8 @@ const initialState = {
   // loading, error, ready, active, finished
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -22,18 +26,35 @@ function reducer(state, action) {
       return { ...state, status: 'error' };
     case 'start':
       return { ...state, status: 'active' };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error('Invalid action');
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     fetch('http://localhost:8000/questions')
@@ -52,7 +73,23 @@ export default function App() {
         {status == 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status == 'active' && <Question question={questions.at(index)} />}
+        {status == 'active' && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions.at(index)}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
     </div>
   );
